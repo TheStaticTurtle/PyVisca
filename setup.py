ec2666111cb2e59efc83a60dc9f89e8360cb635a
TheStaticTurtle <tuglersamuel@gmail.com>
2022-06-20 00:32:27 +0200

    Base files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pyvisca',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python library to interpret and build visca packet',
+    author='TheStaticTurtle',
+    license='GPLv3',
+)
